bfbb06b0265e7f18ddbdc3f46db4ad39aea73ab4
otmiM <89715432+otmiM@users.noreply.github.com>
2024-01-02 15:19:41 -0500

    Update polym_loop.py

--- polym_loop.py
+++ polym_loop.py
@@ -19,9 +19,6 @@
 #
 ################################################################################
 #
-# Polymatic: a general simulated polymerization algorithm
-# Copyright (C) 2013, 2015 Lauren J. Abbott
-#
 # Polymatic is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
 # Foundation, either version 3 of the License, or (at your option) any later
